b6c9fe485d8d67e4daa3e02fca467036a33338c3
nigerabed <nigerabed@gmail.com>
2025-02-12 10:37:38 +0100

    iplayMusic

--- src/app/featured/page.js
+++ src/app/featured/page.js
@@ -19,7 +19,7 @@ export default async function featuredPlay() {
         }
     })
     const data = await response.json()
-    //console.log(data.playlists.items);
+    console.log(data);
 
 
     return (
